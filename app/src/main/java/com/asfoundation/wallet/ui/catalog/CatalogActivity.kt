3db37cd609d3f0f219af455054b684614fbf0436
Davud <work.hosseinidavid@gmail.com>
2019-12-30 17:16:14 +0330

    Add Catalog RecyclerView

--- app/src/main/java/com/asfoundation/wallet/ui/catalog/CatalogActivity.kt
+++ app/src/main/java/com/asfoundation/wallet/ui/catalog/CatalogActivity.kt
@@ -2,7 +2,9 @@ package com.asfoundation.wallet.ui.catalog
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.asf.wallet.R
+import kotlinx.android.synthetic.main.activity_catalog.*
 
 class CatalogActivity : AppCompatActivity() {
 
@@ -10,7 +12,16 @@ class CatalogActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
+    initView()
+  }
+
+  private fun initView() {
+
     setContentView(R.layout.activity_catalog)
+    recyclerCatalog.apply {
+
+      layoutManager = LinearLayoutManager(this@CatalogActivity)
+    }
   }
 
 }
