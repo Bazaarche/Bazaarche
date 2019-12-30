7a3c4c3d2d9f080e00d449f56735cc7a8d82d59d
Davud <work.hosseinidavid@gmail.com>
2019-12-30 14:30:28 +0330

    Add and show CatalogActivity

--- app/src/main/java/com/asfoundation/wallet/router/TransactionsRouter.java
+++ app/src/main/java/com/asfoundation/wallet/router/TransactionsRouter.java
@@ -2,11 +2,11 @@ package com.asfoundation.wallet.router;
 
 import android.content.Context;
 import android.content.Intent;
-import com.asfoundation.wallet.ui.TransactionsActivity;
+import com.asfoundation.wallet.ui.catalog.CatalogActivity;
 
 public class TransactionsRouter {
   public void open(Context context, boolean isClearStack) {
-    Intent intent = new Intent(context, TransactionsActivity.class);
+    Intent intent = new Intent(context, CatalogActivity.class);
     if (isClearStack) {
       intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
     }
---
+++ app/src/main/java/com/asfoundation/wallet/ui/catalog/CatalogActivity.kt
@@ -0,0 +1,16 @@
+package com.asfoundation.wallet.ui.catalog
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.asf.wallet.R
+
+class CatalogActivity : AppCompatActivity() {
+
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    setContentView(R.layout.activity_catalog)
+  }
+
+}
