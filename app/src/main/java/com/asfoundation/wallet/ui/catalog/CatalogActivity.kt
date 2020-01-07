81e774124393c0d58e6a600912ee0dbb9cce4450
Davud <work.hosseinidavid@gmail.com>
2020-01-07 11:42:41 +0330

    Add divider

--- app/src/main/java/com/asfoundation/wallet/ui/catalog/CatalogActivity.kt
+++ app/src/main/java/com/asfoundation/wallet/ui/catalog/CatalogActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.asf.wallet.R
+import com.asfoundation.wallet.ui.getDividerDrawable
+import com.asfoundation.wallet.ui.widget.CatalogDividerItemDecoration
+import com.asfoundation.wallet.ui.widget.CatalogDividerItemDecoration.VERTICAL
 import com.asfoundation.wallet.util.observeNotNull
 import com.asfoundation.wallet.viewmodel.CatalogViewModel
 import com.asfoundation.wallet.viewmodel.CatalogViewModelFactory
@@ -45,11 +49,27 @@ class CatalogActivity : AppCompatActivity() {
   }
 
   private fun setupRecyclerView() {
+
+    fun createItemDecoration(): RecyclerView.ItemDecoration {
+
+      val rightMargin = resources.getDimensionPixelSize(R.dimen.app_image_size)
+      val dividerDrawable = getDividerDrawable(this, 0, rightMargin)
+
+      val itemDecoration = CatalogDividerItemDecoration(this, VERTICAL,
+          R.layout.item_app)
+      itemDecoration.setDrawable(dividerDrawable)
+
+      return itemDecoration
+    }
+
     recyclerCatalog.also {
 
       it.layoutManager = LinearLayoutManager(this)
       it.adapter = adapter
+
+      it.addItemDecoration(createItemDecoration())
     }
+
   }
 
 }
