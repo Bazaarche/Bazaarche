93c3facf84070fb76ebb11eb6411621f3c4df180
Davud <work.hosseinidavid@gmail.com>
2020-02-10 11:11:54 +0330

    Change onBoarding images and add one more page

--- app/src/main/java/com/asfoundation/wallet/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/asfoundation/wallet/ui/onboarding/OnboardingActivity.kt
@@ -261,14 +261,16 @@ class OnboardingActivity : BaseActivity(), OnboardingView {
   private fun createReferralsItemList(maxAmount: String): List<OnboardingItem> {
     val item1 = OnboardingItem(R.string.intro_1_title, this.getString(R.string.intro_1_body))
     val item2 = OnboardingItem(R.string.intro_2_title, this.getString(R.string.intro_2_body))
+    val item3 = OnboardingItem(R.string.intro_3_title, this.getString(R.string.intro_3_body))
 
-    return listOf(item1, item2)
+    return listOf(item1, item2, item3)
   }
 
   private fun createDefaultItemList(): List<OnboardingItem> {
     val item1 = OnboardingItem(R.string.intro_1_title, this.getString(R.string.intro_1_body))
     val item2 = OnboardingItem(R.string.intro_2_title, this.getString(R.string.intro_2_body))
+    val item3 = OnboardingItem(R.string.intro_3_title, this.getString(R.string.intro_3_body))
 
-    return listOf(item1, item2)
+    return listOf(item1, item2, item3)
   }
 }
