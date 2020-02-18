5d01d392432e1b0ee8f833064a57b63170065474
Davud <work.hosseinidavid@gmail.com>
2020-02-18 19:31:30 +0330

    Fix open browser instead of bazaar

--- app/src/main/java/com/asfoundation/wallet/Consts.kt
+++ app/src/main/java/com/asfoundation/wallet/Consts.kt
@@ -1,4 +1,5 @@
 package com.asfoundation.wallet
 
 const val BAZAAR_PACKAGE_NAME = "com.farsitel.bazaar"
-const val BAZAAR_APP_VIEW_URL = "https://www.cafebazaar.ir/app/"
+const val BAZAAR_APP_DETAILS_URL = "https://www.cafebazaar.ir/app/"
+const val BAZAAR_APP_DETAILS_DEEP_LINK = "bazaar://details?id="
