cfe05c999f6832db084a4763ee975b923a325804
Davud <work.hosseinidavid@gmail.com>
2020-02-19 15:09:34 +0330

    Open bazaarche about page on click of help

--- app/src/main/java/com/asfoundation/wallet/Consts.kt
+++ app/src/main/java/com/asfoundation/wallet/Consts.kt
@@ -1,4 +1,5 @@
 package com.asfoundation.wallet
 
+const val BAZAARCHE_ABOUT_URL = "https://cafebazaar.ir/bazaarche-about"
 const val BAZAAR_PACKAGE_NAME = "com.farsitel.bazaar"
 const val BAZAAR_APP_VIEW_URL = "https://www.cafebazaar.ir/app/"
