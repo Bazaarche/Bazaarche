7c7dc28adf56c940c1b5dfe17c28e4b4ce539dc4
Seyyed davud hosseiny <hosseinydavid@gmail.com>
2020-03-03 21:04:37 +0330

    Always check for AdId

--- app/src/main/java/com/asfoundation/wallet/BazaarcheAppSetup.kt
+++ app/src/main/java/com/asfoundation/wallet/BazaarcheAppSetup.kt
@@ -4,8 +4,8 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 import com.asfoundation.wallet.repository.PreferencesDataSource
-import com.asfoundation.wallet.util.languagecontroller.LanguageController
 import com.asfoundation.wallet.util.languagecontroller.Language
+import com.asfoundation.wallet.util.languagecontroller.LanguageController
 import com.google.android.gms.ads.identifier.AdvertisingIdClient
 import io.reactivex.Completable
 import io.reactivex.schedulers.Schedulers
@@ -21,20 +21,14 @@ fun startSetup(application: Application, preferencesDataSource: PreferencesDataS
 @SuppressLint("CheckResult")
 private fun checkAdId(context: Context, preferencesDataSource: PreferencesDataSource) {
 
-  fun isAdIdEmpty(preferencesDataSource: PreferencesDataSource): Boolean {
-    return preferencesDataSource.adId.isEmpty()
-  }
-
   fun determineAdId(context: Context): String {
     val advertisingIdInfo = AdvertisingIdClient.getAdvertisingIdInfo(context)
     return advertisingIdInfo.id
   }
 
   Completable.fromRunnable {
-    if (isAdIdEmpty(preferencesDataSource)) {
-      val adId = determineAdId(context)
-      preferencesDataSource.adId = adId
-    }
+    val adId = determineAdId(context)
+    preferencesDataSource.adId = adId
   }
       .onErrorComplete()
       .subscribeOn(Schedulers.io())
