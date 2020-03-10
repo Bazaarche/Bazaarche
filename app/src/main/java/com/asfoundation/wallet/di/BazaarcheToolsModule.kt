4e37bd6d3712b518f9a432647df39da458529846
Seyyed davood hosseini <hosseinydavid@gmail.com>
2020-03-10 17:21:28 +0330

    Provide NotificationBuilder

--- app/src/main/java/com/asfoundation/wallet/di/BazaarcheToolsModule.kt
+++ app/src/main/java/com/asfoundation/wallet/di/BazaarcheToolsModule.kt
@@ -1,13 +1,48 @@
 package com.asfoundation.wallet.di
 
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import androidx.core.app.NotificationCompat
+import com.asf.wallet.R
+import com.asfoundation.wallet.fcm.FcmService
 import com.asfoundation.wallet.repository.PreferencesDataSource
 import com.asfoundation.wallet.repository.PreferencesDataSourceImpl
+import com.asfoundation.wallet.ui.catalog.CatalogActivity
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 
 @Module
-interface BazaarcheToolsModule {
+abstract class BazaarcheToolsModule {
+
 
   @Binds
-  fun bindsPreferencesDataSource(preferencesDataSource: PreferencesDataSourceImpl): PreferencesDataSource
+  abstract fun bindsPreferencesDataSource(preferencesDataSource: PreferencesDataSourceImpl): PreferencesDataSource
+
+  @Module
+  companion object {
+
+    @JvmStatic
+    @Bazaarche
+    @Provides
+    fun provideNotificationBuilder(context: Context): NotificationCompat.Builder {
+
+      val intent = Intent(context, CatalogActivity::class.java)
+      intent.addFlags( Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+      val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+
+      val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+      return NotificationCompat.Builder(context, FcmService.NOTIFICATION_CHANNEL_ID)
+          .setSmallIcon(R.mipmap.ic_launcher)
+          .setContentTitle(context.getString(R.string.app_name))
+          .setAutoCancel(true)
+          .setSound(defaultSoundUri)
+          .setContentIntent(pendingIntent)
+    }
+
+  }
+
 }
