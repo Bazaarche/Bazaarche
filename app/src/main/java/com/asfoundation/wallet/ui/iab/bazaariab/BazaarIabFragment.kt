d3a8624546dc8562f85f147a527ad75a047da532
Seyyed davood hosseini <hosseinydavid@gmail.com>
2020-03-25 19:03:04 +0430

    Add View for BazaarIabFragment and init its views

--- app/src/main/java/com/asfoundation/wallet/ui/iab/bazaariab/BazaarIabFragment.kt
+++ app/src/main/java/com/asfoundation/wallet/ui/iab/bazaariab/BazaarIabFragment.kt
@@ -3,8 +3,14 @@ package com.asfoundation.wallet.ui.iab.bazaariab
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.airbnb.lottie.LottieAnimationView
+import com.asf.wallet.R
 import com.asfoundation.wallet.entity.TransactionBuilder
 import com.asfoundation.wallet.ui.iab.IabView
 import com.phelat.poolakey.Payment
@@ -13,6 +19,7 @@ import com.phelat.poolakey.rx.onActivityResult
 import com.phelat.poolakey.rx.purchaseProduct
 import dagger.android.support.DaggerFragment
 import io.reactivex.disposables.Disposable
+import kotlinx.android.synthetic.main.fragment_bazaar_iab.*
 import javax.inject.Inject
 
 
@@ -37,6 +44,10 @@ class BazaarIabFragment : DaggerFragment() {
   }
 
   private lateinit var iabView: IabView
+
+  private lateinit var transactionCompleteAnimationView: LottieAnimationView
+  private lateinit var errorMessage: TextView
+  private lateinit var errorOkButton: View
 
   @Inject
   lateinit var bazaarIabViewModelFactory: BazaarIabViewModelFactory
@@ -64,6 +75,18 @@ class BazaarIabFragment : DaggerFragment() {
     connectPayment()
   }
 
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                            savedInstanceState: Bundle?): View? {
+
+    return inflater.inflate(R.layout.fragment_bazaar_iab, container, false)
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    errorMessage = view.findViewById(R.id.activity_iab_error_message)
+    errorOkButton = view.findViewById(R.id.activity_iab_error_ok_button)
+    transactionCompleteAnimationView = view.findViewById(R.id.lottie_transaction_success)
+  }
 
   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
     val purchaseResult = payment.onActivityResult(requestCode, resultCode, data)
