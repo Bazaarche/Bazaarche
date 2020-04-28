94b1c8ff69f77809da108e49510433eadb3e716f
Seyyed davood hosseini <hosseinydavid@gmail.com>
2020-04-28 17:13:58 +0430

    Operate navigate up to pop fragments backStack

--- app/src/main/java/com/asfoundation/wallet/ui/bazarchesettings/BazaarcheSettingsActivity.kt
+++ app/src/main/java/com/asfoundation/wallet/ui/bazarchesettings/BazaarcheSettingsActivity.kt
@@ -1,19 +1,29 @@
 package com.asfoundation.wallet.ui.bazarchesettings
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.asf.wallet.R
-import com.asfoundation.wallet.ui.BaseActivity
+import kotlinx.android.synthetic.main.layout_app_bar.*
 
-class BazaarcheSettingsActivity : BaseActivity() {
+class BazaarcheSettingsActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_bazaarche_settings)
-    toolbar()
+    setupActionbar()
 
     supportFragmentManager.beginTransaction()
         .replace(R.id.fragment_container, BazaarcheSettingsFragment())
         .commit()
   }
 
+  private fun setupActionbar() {
+    setSupportActionBar(toolbar)
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+  }
+
+  override fun onSupportNavigateUp(): Boolean {
+    onBackPressed()
+    return true
+  }
 }
