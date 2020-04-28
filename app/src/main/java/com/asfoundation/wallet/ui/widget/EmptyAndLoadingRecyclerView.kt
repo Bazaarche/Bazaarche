1b255160b2afd08a2f6a2957df8e45e62d92e8d3
Seyyed davood hosseini <hosseinydavid@gmail.com>
2020-04-28 16:19:11 +0430

    Add EmptyAndLoadingRecyclerView for easily manage empty and loading state of a list

---
+++ app/src/main/java/com/asfoundation/wallet/ui/widget/EmptyAndLoadingRecyclerView.kt
@@ -0,0 +1,65 @@
+package com.asfoundation.wallet.ui.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class EmptyAndLoadingRecyclerView : RecyclerView {
+
+  var emptyView: View? = null
+    set(value) {
+      field = value
+      checkEmptyAndLoading()
+    }
+
+  private val isEmpty: Boolean
+    get() {
+      return adapter?.itemCount ?: 0 <= 0
+    }
+
+  var loadingView: View? = null
+    set(value) {
+      field = value
+      checkEmptyAndLoading()
+    }
+
+  var isLoading: Boolean = false
+    set(value) {
+      field = value
+      checkEmptyAndLoading()
+    }
+
+  private val observer: AdapterDataObserver = object : AdapterDataObserver() {
+    override fun onChanged() {
+      checkEmptyAndLoading()
+    }
+  }
+
+  constructor(context: Context) : super(context)
+  constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+  constructor(context: Context, attrs: AttributeSet?, defStyle: Int) :
+      super(context, attrs, defStyle)
+
+  override fun setAdapter(adapter: Adapter<*>?) {
+    getAdapter()?.unregisterAdapterDataObserver(observer)
+    super.setAdapter(adapter)
+    adapter?.registerAdapterDataObserver(observer)
+  }
+
+  private fun checkEmptyAndLoading() {
+    if (isEmpty) {
+      if (isLoading) {
+        loadingView?.visibility = View.VISIBLE
+        emptyView?.visibility = View.GONE
+      } else {
+        emptyView?.visibility = View.VISIBLE
+        loadingView?.visibility = View.GONE
+      }
+    } else {
+      emptyView?.visibility = View.GONE
+      loadingView?.visibility = View.GONE
+    }
+  }
+
+}
