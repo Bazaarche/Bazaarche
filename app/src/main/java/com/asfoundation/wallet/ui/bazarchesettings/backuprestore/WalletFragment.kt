16837609dd73f4bdcdd81ef730a009a996b7a984
Seyyed davood hosseini <hosseinydavid@gmail.com>
2020-06-18 19:49:49 +0430

    Implement WalletFragment layout design

--- app/src/main/java/com/asfoundation/wallet/ui/bazarchesettings/backuprestore/WalletFragment.kt
+++ app/src/main/java/com/asfoundation/wallet/ui/bazarchesettings/backuprestore/WalletFragment.kt
@@ -1,5 +1,17 @@
 package com.asfoundation.wallet.ui.bazarchesettings.backuprestore
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.asf.wallet.R
 
-class WalletFragment : Fragment()
+class WalletFragment : Fragment() {
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                            savedInstanceState: Bundle?): View? {
+
+    return inflater.inflate(R.layout.fragment_wallet, container, false)
+  }
+}
