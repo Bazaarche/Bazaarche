1736d0c2d53baf6c072050f4e44b5362fec1741e
Seyyed davood hosseini <hosseinydavid@gmail.com>
2020-06-20 11:52:38 +0430

    Open import wallet page from WalletFragment

--- app/src/main/java/com/asfoundation/wallet/ui/bazarchesettings/backuprestore/WalletFragment.kt
+++ app/src/main/java/com/asfoundation/wallet/ui/bazarchesettings/backuprestore/WalletFragment.kt
@@ -1,15 +1,20 @@
 package com.asfoundation.wallet.ui.bazarchesettings.backuprestore
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProviders
 import com.asf.wallet.R
+import com.asfoundation.wallet.C.IMPORT_REQUEST_CODE
 import com.asfoundation.wallet.entity.Wallet
+import com.asfoundation.wallet.ui.ImportWalletActivity
 import com.asfoundation.wallet.util.observe
 import com.asfoundation.wallet.viewmodel.WalletsViewModel
 import com.asfoundation.wallet.viewmodel.WalletsViewModelFactory
+import com.google.android.material.snackbar.Snackbar
 import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.fragment_wallet.*
 import javax.inject.Inject
@@ -34,11 +39,40 @@ class WalletFragment : DaggerFragment() {
 
     requireActivity().setTitle(R.string.wallet)
     observeViewModels()
+    setClickListeners()
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    if (requestCode == IMPORT_REQUEST_CODE && resultCode == RESULT_OK) {
+
+      onImportSucceeded()
+    }
   }
 
   private fun observeViewModels() {
     viewModel.defaultWallet()
         .observe(viewLifecycleOwner, ::onWalletReady)
+  }
+
+  private fun setClickListeners() {
+
+    fun startImportWalletActivity() {
+      val intent = Intent(requireContext(), ImportWalletActivity::class.java)
+      startActivityForResult(intent, IMPORT_REQUEST_CODE)
+    }
+
+    textRestore.setOnClickListener {
+      startImportWalletActivity()
+    }
+  }
+
+  private fun onImportSucceeded() {
+    Snackbar.make(requireView(),
+        R.string.toast_message_wallet_imported,
+        Snackbar.LENGTH_SHORT)
+        .show()
+
+    viewModel.showTransactions(requireContext())
   }
 
   private fun onWalletReady(wallet: Wallet) {
